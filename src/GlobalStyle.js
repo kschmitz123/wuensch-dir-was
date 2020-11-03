6fa0ef149968fec30f36f13c4f041b377fdd08b3
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-03 11:46:44 +0100

    Update  GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *,*::after,*::before {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
