6707ff67db5f902bed28378de9616795d6a0ebab
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-03 17:05:20 +0100

    Replace button with Link and svg

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import PropTypes from 'prop-types';
-import Button from '../components/Button';
 import WishListItem from '../components/WishListItem';
 
 const WishListItems = styled.div``;
+
+const NavLink = styled(Link)`
+  display: inline-grid;
+  place-content: center;
+  text-decoration: none;
+  height: 50px;
+  width: 50px;
+  background-color: #ff9a9e;
+  border-radius: 50%;
+  cursor: pointer;
+`;
 
 const Container = styled.div`
   text-align: center;
@@ -17,9 +27,16 @@ const Welcome = ({ title }) => {
         <WishListItem title={title} />
         <WishListItem title={title} />
       </WishListItems>
-      <Button>
-        <Link to="/add">+</Link>
-      </Button>
+      <NavLink to="/add">
+        <svg
+          xmins="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+        >
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />{' '}
+        </svg>
+      </NavLink>
     </Container>
   );
 };
