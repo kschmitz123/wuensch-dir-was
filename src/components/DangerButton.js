d68e5a1a1d3178adf883032c4863e8cd1f52d28c
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-05 11:08:48 +0100

    🍱  Add delete button

---
+++ src/components/DangerButton.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+const DangerButton = styled.button`
+  background-color: red;
+  font-size: 1.3rem;
+  border-radius: 25px;
+  border: none;
+  padding: 6px;
+`;
+
+export default DangerButton;
